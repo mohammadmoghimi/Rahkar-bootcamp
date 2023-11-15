4be080b838a883230c6c5c108a38eba5fd309161
mohammadmoghimi <mmoghimi2003@gmail.com>
2023-11-15 21:39:44 +0330

    removed all js files

--- assignment-2/2nd.js
+++
@@ -1,3 +0,0 @@
-for (let i = 100; i <= 1000; i = i + 2) {
-    console.log(i);
-}
--- assignment-2/3rd.js
+++
@@ -1,8 +0,0 @@
-let powerOfTwo , powerOfThree  , root = 0;
-
-for( let i = 0 ; i <= 20 ; i ++){
-    powerOfTwo = i*i ;
-    powerOfThree = powerOfTwo * i ;
-    root = Math.pow(i,1/2)
-    console.log(powerOfTwo , powerOfThree , root);
-} 
